547dda50a68f82496472f4a8e8fd419f6d0897e7
ELLITNOW <emmanuelj90@gmail.com>
2025-11-16 19:43:35 +0100

    Create ellit_leaflet.py

---
+++ components/ellit_leaflet/ellit_leaflet.py
@@ -0,0 +1,6 @@
+# Ellit Leaflet Component Loader
+# Este archivo es requerido por Streamlit para registrar correctamente el componente.
+
+from . import ellit_leaflet_map
+
+__all__ = ["ellit_leaflet_map"]
