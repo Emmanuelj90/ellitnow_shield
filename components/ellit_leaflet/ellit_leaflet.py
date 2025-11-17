31769c0490a90e74e266ab889713868b8840a7fb
ELLITNOW <emmanuelj90@gmail.com>
2025-11-17 18:54:48 +0100

    Update ellit_leaflet.py

--- components/ellit_leaflet/ellit_leaflet.py
+++ components/ellit_leaflet/ellit_leaflet.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 from pathlib import Path
 
 # Path al frontend (index.html)
-_component_path = Path(__file__).parent / "frontend"
+_component_path = Path(__file__).parent / "frontend
 
 # Declarar componente con nombre único
 ellit_map_component = components.declare_component(
