9c1420e62523817d51be4cdf91de391512df1662
ELLITNOW <emmanuelj90@gmail.com>
2025-11-17 21:29:27 +0100

    Update component.py

--- components/ellit_leaflet/component.py
+++ components/ellit_leaflet/component.py
@@ -2,11 +2,13 @@ from pathlib import Path
 import streamlit.components.v1 as components
 
 # Ruta al frontend
-parent_path = str(Path(__file__).parent / "frontend")
+frontend_dir = str(Path(__file__).parent / "frontend")
 
-# Declaramos el componente
-ellitmap_component = components.declare_component("ellitmap", path=parent_path)
+# Declaración del componente real
+ellitmap_component = components.declare_component(
+    "ellitmap",
+    path=frontend_dir
+)
 
-# Función pública
 def show_map(data: dict, key=None):
     return ellitmap_component(data=data, key=key)
