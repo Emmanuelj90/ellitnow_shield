ca95d0ac468c344f7eb95b03281c1375a97dbb31
ELLITNOW <emmanuelj90@gmail.com>
2025-11-17 14:21:45 +0100

    Update __init__.py

--- components/ellit_leaflet/__init__.py
+++ components/ellit_leaflet/__init__.py
@@ -1,25 +1,12 @@
 import os
-import json
 import streamlit.components.v1 as components
 
-# Ruta al frontend
-COMPONENT_PATH = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "frontend")
-)
+COMPONENT_PATH = os.path.join(os.path.dirname(__file__), "frontend")
 
-# Declaración del componente (name = "ellit_leaflet")
-ellit_leaflet_component = components.declare_component(
+ellit_leaflet = components.declare_component(
     "ellit_leaflet",
     path=COMPONENT_PATH
 )
 
-def ellit_leaflet_map(threat_data: dict, height: int = 600, key: str = None):
-    """
-    Wrapper del componente Leaflet para Streamlit.
-    """
-    return ellit_leaflet_component(
-        threatData=json.dumps(threat_data),
-        height=height,
-        key=key
-    )
-
+def show_map(threatData):
+    return ellit_leaflet(threatData=threatData)
