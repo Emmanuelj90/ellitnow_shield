cd372ae5f67422356d1bd8861bc65c7343344618
ELLITNOW <emmanuelj90@gmail.com>
2025-11-17 17:07:11 +0100

    Update ellit_leaflet.py

--- components/ellit_leaflet/ellit_leaflet.py
+++ components/ellit_leaflet/ellit_leaflet.py
@@ -1,13 +1,11 @@
-import streamlit as st
 import streamlit.components.v1 as components
-from pathlib import Path
-import json
+import os
 
-# Declaración CORRECTA del componente
-ellit_map_component = components.declare_component(
-    "ellitmap",  # 🔥 nombre único, NO igual al archivo ni carpeta
-    path=str(Path(__file__).parent / "frontend")  # ruta al index.html
+# Declarar componente correctamente
+_component_func = components.declare_component(
+    "ellit_leaflet",  
+    path=os.path.join(os.path.dirname(__file__), "frontend")
 )
 
-def show_map(data: dict):
-    return ellit_map_component(data=data)
+def show_map(data: dict, key=None):
+    return _component_func(data=data, key=key)
