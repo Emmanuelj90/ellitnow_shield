96271b92909ab9d58d1b33f5400d76d155a45d2d
ELLITNOW <emmanuelj90@gmail.com>
2025-11-17 18:21:33 +0100

    Update ellit_leaflet.py

--- components/ellit_leaflet/ellit_leaflet.py
+++ components/ellit_leaflet/ellit_leaflet.py
@@ -8,9 +8,9 @@ _component_path = Path(__file__).parent / "frontend"
 # Declarar componente con nombre único
 ellit_map_component = components.declare_component(
     "ellitmap",
-    path=str(Path(__file__).parent / "frontend")
+    path=str(_component_path)
 )
 
 # Función pública para usar en app.py
 def show_map(data: dict, key=None):
-    return ellitmap_component(data=data, key=key)
+    return ellit_map_component(data=data, key=key)
